2cf59eb42ace34b241568be053ed9fb14e9b4083
Jordan Strande <jstrande@rams.colostate.edu>
2022-12-13 11:12:17 -0700

    Updated menu

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -13,9 +13,9 @@ export function Navbar() {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to="/#Home" className="navbar-logo" onClick={closeMobileMenu}>
+                    {/* <Link to="/#Home" className="navbar-logo" onClick={closeMobileMenu}>
                        JS <i className='fab fab-typo3' />
-                    </Link>
+                    </Link> */}
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
                     </div>
