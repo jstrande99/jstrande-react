cbe3958dd9ff5e43bb617e36f2c556e66820f80f
Jordan Strande <jstrande@rams.colostate.edu>
2023-01-13 12:03:09 -0700

    Updated Loading Page

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Loader.css';
 import Floating from '../Pages/images/floatin1.png';
-import Hole from '../Pages/images/blackHole.png';
 
 export function Loader(){
     const [Loading, setLoading] = useState("false");
@@ -9,7 +8,7 @@ export function Loader(){
         setLoading("true")
         setTimeout(() => {
         setLoading("false");
-        }, 2500);
+        }, 1100);
     }, [setLoading]); 
 
 
@@ -18,10 +17,6 @@ export function Loader(){
             <div className='innerSpin'>
                 <h1 className='loadwords'>WELCOME</h1>
                 <img src={Floating} alt='Astronat' className='loadImg'/>
-                <h1 className='loadwords'>
-                    CREATED WITH REACT
-                </h1>
-                <img src={Hole} alt='Blackhole' className='loadBackImg'/>
             </div>
         </div>
     )
