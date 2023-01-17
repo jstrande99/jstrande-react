ff9c4a3b61ff4b15d4ce6c15028bf04999af6229
Jordan Strande <jstrande@rams.colostate.edu>
2023-01-17 15:57:11 -0700

    Updated Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -13,9 +13,6 @@ export function Navbar() {
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    {/* <Link to="/#Home" className="navbar-logo" onClick={closeMobileMenu}>
-                       JS <i className='fab fab-typo3' />
-                    </Link> */}
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
                     </div>
@@ -26,15 +23,30 @@ export function Navbar() {
                             </Link>
                         </li>
                         <li className='nav-item'>
+                            <Link to='/#About' className='nav-links' onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/#Languages' className='nav-links' onClick={closeMobileMenu}>
+                                Experience
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/#Studies' className='nav-links' onClick={closeMobileMenu}>
+                                Studies
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
                             <Link to='/#Work' className='nav-links' onClick={closeMobileMenu}>
                                 Work
                             </Link>
                         </li>
-                        {/* <li className='nav-item'>
-                            <Link to='/studies' className='nav-links' onClick={closeMobileMenu}>
-                                Studies
+                        <li className='nav-item'>
+                            <Link to='/#Studies' className='nav-links' onClick={closeMobileMenu}>
+                                Projects
                             </Link>
-                        </li> */}
+                        </li>
                     </ul>
                 </div>
             </nav>
