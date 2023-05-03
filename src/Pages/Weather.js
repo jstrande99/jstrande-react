e7e8bb4cf9aaeb4c96dfe1034d2c8e47bd9eb637
Jordan Strande <jstrande@rams.colostate.edu>
2023-05-03 17:32:30 -0600

    cleaned up code

--- src/Pages/Weather.js
+++ src/Pages/Weather.js
@@ -19,34 +19,66 @@ export function Weather(process){
                 <form className="inputZip">
                     <label className="zipLabel">
                         Zip Code:
-                        <input className="inputBox" type="text" onChange={e => getWeather(e.target.value)} maxLength="5"/>
+                        <input 
+                            className="inputBox" 
+                            type="text" 
+                            onChange={e => getWeather(e.target.value)} 
+                            maxLength="5"
+                        />
                     </label>
                 </form>
                 <div className="output">
-                    <h2 className="locationName">Invalid Zip Code</h2>
+                    <h2 className="locationName">
+                        Invalid Zip Code
+                    </h2>
                 </div>
             </div>
         );
     }else{
         return (
             <div className="container">
-                <h1 className="check">Get Your Weather</h1>
+                <h1 className="check">
+                    Get Your Weather
+                </h1>
                 <form className="inputZip">
                     <label className="zipLabel">
                         Zip Code:
-                        <input className="inputBox" type="text" onChange={e => getWeather(e.target.value)} maxLength="5"/>
+                        <input 
+                            className="inputBox" 
+                            type="text" 
+                            onChange={e => getWeather(e.target.value)} 
+                            maxLength="5"
+                        />
                     </label>
                 </form>
                 {weatherData.name && (
                 <div className="output">
-                    <h2 className="locationName">{weatherData.name}'s weather</h2>
-                    <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="icons" className="weatherIcon1"/>
-                    <p>{weatherData.main.temp}&#176;</p>
-                    <p>Feels like {weatherData.main.feels_like}&#176;</p>
-                    <p>Low of {weatherData.main.temp_min}&#176; | High of {weatherData.main.temp_max}&#176;</p>
-                    <p>{weatherData.main.humidity}% humidity</p>
-                    <p>{weatherData.weather[0].description}</p>
-                    <p>Wind is {weatherData.wind.speed} mph</p>
+                    <h2 className="locationName">
+                        {weatherData.name}'s weather
+                    </h2>
+                    <img 
+                        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} 
+                        alt="icons" 
+                        className="weatherIcon1"
+                    />
+                    <p>
+                        {weatherData.main.temp}&#176;
+                    </p>
+                    <p>
+                        Feels like {weatherData.main.feels_like}&#176;
+                    </p>
+                    <p>
+                        Low of {weatherData.main.temp_min}&#176; | High of {weatherData.main.temp_max}&#176;
+                    </p>
+                    <p>
+                        {weatherData.main.humidity}% humidity
+                    </p>
+                    <p>
+                        {weatherData.weather[0].description}
+                    </p>
+                    <p>
+                        Wind is {weatherData.wind.speed} mph
+                    </p>
                 </div>
                 )}
             </div>
